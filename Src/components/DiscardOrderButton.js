bf1d39f34b6fda79295de88c45c6700864c32a53
Devmobilesutra <developer@mobilesutra.com>
2021-01-11 15:27:31 +0530

    shops

--- Src/components/DiscardOrderButton.js
+++ Src/components/DiscardOrderButton.js
@@ -1,32 +1,38 @@
-
-import React, { Component } from 'react';
-import {StyleSheet, View, Text, Image, ImageBackground, TouchableOpacity, ScrollView} from 'react-native';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 export default class DiscardOrderButton extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  };
-    }
-    render() {
-        return (
-            <View style= {styles.buttonContainer}>
-                    <View style={styles.button}>
-                        <Text style={styles.buttonText} >DISCARD</Text>
-                    </View>
-            </View>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <View style={styles.buttonContainer}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>DISCARD</Text>
+        </View>
+      </View>
+    );
+  }
 }
 
-
-
-
 const styles = StyleSheet.create({
-
   buttonContainer: {
-      alignItems: 'flex-end',
-      flexDirection:'column'
+    alignItems: 'flex-end',
+    flexDirection: 'column',
   },
 
   //  button: {
@@ -43,30 +49,26 @@ const styles = StyleSheet.create({
   //     marginVertical: wp('1'),
   //   },
 
-     button: {
-      width: wp('47'),
-      height: hp('8'),
-      backgroundColor: '#46BE50',
-      paddingVertical: 15,
-      justifyContent: 'center',
-      marginLeft: hp('1'),
+  button: {
+    width: wp('47'),
+    height: hp('8'),
+    backgroundColor: '#46BE50',
+    paddingVertical: 15,
+    justifyContent: 'center',
+    marginLeft: hp('1'),
     //   marginHorizontal: hp('1'),
-      borderRadius: wp('2'),
-      marginVertical: wp('1'),
-    },
+    borderRadius: wp('2'),
+    marginVertical: wp('1'),
+  },
 
-
-   
-    buttonText: {
-      fontSize: 16,
-      // color: '#796A6A',
-      color: '#ffffff',
-      textAlign: 'center',
-      alignItems: 'flex-end',
-      justifyContent: 'center',
-      fontWeight: 'bold',
-      fontFamily: 'Proxima Nova',
-      
-    },
-     
+  buttonText: {
+    fontSize: 16,
+    // color: '#796A6A',
+    color: '#ffffff',
+    textAlign: 'center',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    fontWeight: 'bold',
+    fontFamily: 'Proxima Nova',
+  },
 });
